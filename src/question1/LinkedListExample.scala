9c692adb160b2838e2275f81b5f79f45b9467614
Alessandro Baccarini <baccaan@gmail.com>
2021-07-20 17:26:16 -0400

    initial commit

---
+++ src/question1/LinkedListExample.scala
@@ -0,0 +1,37 @@
+package question1
+
+object LinkedListExample {
+
+  def main(args: Array[String]): Unit = {
+
+    // create the list [5, 3, 1] starting at the end and prepending each value
+    var myList: LinkedListNode[Int] = new LinkedListNode[Int](1, null)
+    myList = new LinkedListNode[Int](3, myList)
+    myList = new LinkedListNode[Int](5, myList)
+
+    // prints 1
+    println(myList.next.next.value)
+
+
+    myList = myList.prepend(10)
+    myList = myList.prepend(11)
+    myList = myList.prepend(12)
+
+    println(myList)
+
+
+    println(myList.apply(4).value)
+    println(myList.find(5).value)
+    println(myList.findIterative(5).value)
+
+
+    val squareFunction = (x: Int) => x * x
+    val newList = myList.map(squareFunction)
+    println(newList)
+
+
+//    equipmentExample()
+  }
+
+
+}
---
+++ src/question2/StringActor.scala
@@ -0,0 +1,7 @@
+package question2
+
+import akka.actor._
+
+class StringActor (aString:String) extends Actor {
+  override def receive: Receive = ???
+}
